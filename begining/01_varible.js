36ef1c32df7895d13246264e9b5df0275e1a1e1e
abhi9813 <112932390+abhi9813@users.noreply.github.com>
2025-06-29 14:10:49 +0000

    varible knowlege

---
+++ begining/01_varible.js
@@ -0,0 +1,8 @@
+const accountId = 143321
+let accountEmail = "downtoearth01@gmail.com"
+var accountPassword = "Johan@935"
+accountCity = "jharkhand"
+let accountState;
+
+console.log("accountId");
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
